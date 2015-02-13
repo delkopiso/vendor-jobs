1bea57181615f8003d674284b8bb424ffce16805
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-02-12 20:15:02 -0500

    created scrapers for each tech source each inheriting from BaseScraper

---
+++ tech_scrapers.py
@@ -0,0 +1,31 @@
+from base_scraper import BaseScraper
+
+
+class TodayNGScraper(BaseScraper):
+    def __init__(self, db_collection):
+        BaseScraper.__init__(self, api_id="2yio1io4", category="Tech", db_collection=db_collection,
+                             logo="http://static.today.ng/wp-content/uploads/2014/09/logo.jpg")
+
+    def parse_title(self, piece):
+        self.title = piece["title"]["text"].encode("utf-8")
+
+    def parse_text_body(self, piece):
+        self.body_text = piece['text']
+
+    def parse_cover_picture(self, piece):
+        self.cover_picture = piece['coverPic']['src']
+
+
+class TechCabalScraper(BaseScraper):
+    def __init__(self, db_collection):
+        BaseScraper.__init__(self, api_id="3lfnkuxu", category="Tech", db_collection=db_collection,
+                             logo="http://www.techcabal.com/wp-content/uploads/2013/09/tclogobig.png")
+
+    def parse_title(self, piece):
+        self.title = piece['title']['text'].encode('utf-8')
+
+    def parse_text_body(self, piece):
+        self.body_text = piece['text'].encode('utf-8')
+
+    def parse_cover_picture(self, piece):
+        self.cover_picture = piece['pic']['src'].encode('utf-8')
