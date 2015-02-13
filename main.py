dd2abc5c7fc82fe9b141e1f6f20d61535b48f1fb
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-02-13 04:49:38 -0500

    switch to in-memory jobstore

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
 from pytz import timezone
-from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
-from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+#from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.executors.pool import ProcessPoolExecutor
 import logging
@@ -11,7 +10,6 @@ from popular_scrapers import BellaNaijaScraper
 from tech_scrapers import TechCabalScraper
 
 logging.basicConfig()
-DB_NAME = "heroku_app33940774"
 ARTICLE_COLLECTION = "article"
 scrapers = []
 
@@ -31,46 +29,33 @@ def run_scrapers():
 
 
 def scrape():
-    print "Starting scraper..."
-    print "Fetching MONGOLAB_URI..."
     uri = os.environ.get('MONGOLAB_URI')
-    print "Acquired {0} from the system as the connection string".format(uri)
     db_connection = MongoClient(host=uri) if uri is not None else MongoClient()
-    print "Database connection established"
-    db = db_connection[DB_NAME]
+    db = db_connection.get_default_database()
     collection = db[ARTICLE_COLLECTION]
-    print "Defining scrapers..."
     define_scrapers(collection)
-    print "Running each scraper..."
+    print "Running scrapers..."
     run_scrapers()
-
-
-# def tick():
-#     print('Tick! The time is: %s' % datetime.now())
 
 
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
     url = os.environ.get('DATABASE_URL')
-    jobstores = {}
-    if url != None:
-        jobstores = {
-            'default': SQLAlchemyJobStore(url=url)
-        }
-    else:
-        jobstores = {
-            'default': MemoryJobStore()
-        }
+    jobstores = {'default': MemoryJobStore()}
+    # if url != None:
+    #     jobstores = {
+    #         'default': SQLAlchemyJobStore(url=url)
+    #     }
+    # else:
+    #     jobstores = {
+    #         'default': MemoryJobStore()
+    #     }
     executors = {
         'processpool': ProcessPoolExecutor(max_workers=1)
     }
-    job_defaults = {
-        'coalesce': True
-    }
-    scheduler.configure(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=timezone('US/Eastern'))
-    # scheduler.add_job(tick, 'interval', seconds=3, id='test_timer_tick')
-    scheduler.add_job(scrape, 'interval', minutes=3, start_date='2015-2-13 03:58')#day_of_week='mon-sun', hour=3)
-    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+    scheduler.configure(jobstores=jobstores, executors=executors, timezone=timezone('US/Eastern'))
+    scheduler.add_job(scrape, 'interval', days=1, start_date='2015-2-13 05:00')
+    print 'Scrape job has been scheduled'
 
     try:
         scheduler.start()
