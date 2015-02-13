a9330222f45183df894aa7b00352a219ce4b4324
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-02-12 23:58:23 -0500

    adding scrape job as cron

--- main.py
+++ main.py
@@ -1,44 +1,43 @@
 import os
-from pytz import utc
+from pytz import timezone
 from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.executors.pool import ProcessPoolExecutor
 
-# from pymongo.mongo_client import MongoClient
-# from popular_scrapers import BellaNaijaScraper
-# from tech_scrapers import TechCabalScraper
-#
-# DB_NAME = "vendor"
-# ARTICLE_COLLECTION = "article"
-# scrapers = []
-#
-#
-# def define_scrapers(collection):
-#     b_naij = BellaNaijaScraper(collection)
-#     scrapers.append(b_naij)
-#     tech_cabal = TechCabalScraper(collection)
-#     scrapers.append(tech_cabal)
-#
-#
-# def run_scrapers():
-#     for scraper in scrapers:
-#         scraper.run()
-#
-#
-# @scheduler.scheduled_job('cron', day_of_week='mon-sun', hour=23, id='main_scrape_job')
-# def main_job():
-#     uri = os.environ.get('MONGOLAB_URI')
-#     db_connection = MongoClient(host=uri) if uri is not None else MongoClient()
-#     db = db_connection[DB_NAME]
-#     collection = db[ARTICLE_COLLECTION]
-#     define_scrapers(collection)
-#     run_scrapers()
+from pymongo.mongo_client import MongoClient
+from popular_scrapers import BellaNaijaScraper
+from tech_scrapers import TechCabalScraper
+
+DB_NAME = "vendor"
+ARTICLE_COLLECTION = "article"
+scrapers = []
 
 
-def tick():
-    print('Tick! The time is: %s' % datetime.now())
+def define_scrapers(collection):
+    b_naij = BellaNaijaScraper(collection)
+    scrapers.append(b_naij)
+    tech_cabal = TechCabalScraper(collection)
+    scrapers.append(tech_cabal)
+
+
+def run_scrapers():
+    for scraper in scrapers:
+        scraper.run()
+
+
+def scrape():
+    uri = os.environ.get('MONGOLAB_URI')
+    db_connection = MongoClient(host=uri) if uri is not None else MongoClient()
+    db = db_connection[DB_NAME]
+    collection = db[ARTICLE_COLLECTION]
+    define_scrapers(collection)
+    run_scrapers()
+
+
+# def tick():
+#     print('Tick! The time is: %s' % datetime.now())
 
 
 if __name__ == '__main__':
@@ -59,8 +58,9 @@ if __name__ == '__main__':
     job_defaults = {
         # 'coalesce': True
     }
-    scheduler.configure(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=utc)
+    scheduler.configure(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=timezone('US/Eastern'))
     scheduler.add_job(tick, 'interval', seconds=3, id='test_timer_tick')
+    # scheduler.add_job(scrape, 'cron', day_of_week='mon-sun', hour=0, id='main_scrape_job')
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
     try:
