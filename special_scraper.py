20bb2371bd81dc9ae7bfce158517355bd4794e1c
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-03-08 20:34:39 -0400

    Added specialty scraper child classes for troublesome sources

---
+++ special_scraper.py
@@ -0,0 +1,55 @@
+import datetime
+import requests
+from scraper import Scraper
+from BeautifulSoup import BeautifulSoup as BS
+
+
+class SaharaScraper(Scraper):
+    def __init__(self, name, api_id, category, db_collection, logo=""):
+        Scraper.__init__(self, name, api_id, category, db_collection, logo)
+
+    def retrieve_picture(self, source):
+        r = requests.get(source)
+        content = r.content
+        soup = BS(content)
+        content_div = soup.findAll('div', {'class': 'story-content'})
+        content = BS(str(content_div))
+        images = content.findAll('img')
+        pictures = []
+        cover_picture = ""
+        for x in range(0, len(images)):
+            b = str(images[x])
+            b = b.split('src="')
+            b = b[1]
+            b = b.split('"')
+            b = b[0]
+            pictures.append(b)
+        if len(pictures) > 0:
+            cover_picture = pictures[0]
+        if len(cover_picture) < 3:
+            cover_picture = ""
+        return cover_picture
+
+    def run(self):
+        results = self.load_data()
+        count = 0
+        while count < results["count"]:
+            piece = results["results"]["collection1"][count]
+            title = piece["title"]["text"].encode("utf-8")
+            source = str(piece["title"]["href"])
+            cover_pic = self.retrieve_picture(source)
+            if self.db_collection.find({"source": source}).count() != 0:
+                return
+            else:
+                self.db_collection.insert({
+                    "title": title,
+                    "source": source,
+                    "coverPic": cover_pic,
+                    "section": self.category,
+                    "logo": self.logo,
+                    "popularity": 0,
+                    "mixIndex": self.mix_index,
+                    "dateAdded": datetime.datetime.now()
+                })
+            self.mix_index = count % results["count"]
+            count += 1
