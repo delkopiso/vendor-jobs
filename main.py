aafecd5c48b50c91f92c39fa72ff7fe860c43be9
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-03-08 20:38:00 -0400

    Compiled all the sources into the definition method

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import os
 from apscheduler.events import EVENT_JOB_ERROR, EVENT_JOB_EXECUTED
 from pytz import timezone
 from apscheduler.schedulers.blocking import BlockingScheduler
-#from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.executors.pool import ProcessPoolExecutor
 import logging
 from pymongo.mongo_client import MongoClient
-from popular_scrapers import BellaNaijaScraper
-from tech_scrapers import TechCabalScraper
+from scraper import Scraper
+from special_scraper import SaharaScraper
 
 logging.basicConfig()
 ARTICLE_COLLECTION = "article"
@@ -16,12 +15,69 @@ scrapers = []
 
 
 def define_scrapers(collection):
-    print "Defining Bella Naija scraper..."
-    b_naij = BellaNaijaScraper(collection)
+    # Gossip sources
+    b_naij = Scraper("Bella Naija", api_id="arepnqw8", category="Gossip", db_collection=collection,
+                     logo="http://www.bellanaija.com/wp-content/themes/diamonds/images/bellanaija-mobile.png")
+    print "Adding " + b_naij.get_name() + " scraper..."
     scrapers.append(b_naij)
-    print "Defining Tech Cabal scraper..."
-    tech_cabal = TechCabalScraper(collection)
+    ndani = Scraper("Ndani TV", api_id="8ms0cfmi", category="Gossip", db_collection=collection,
+                    logo="http://ndani.tv/images/logo.png")
+    print "Adding " + ndani.get_name() + " scraper..."
+    scrapers.append(ndani)
+    y_naija_gossip = Scraper("Y Naija", api_id="2nf95wdg", category="Gossip", db_collection=collection,
+                             logo="http://ynaija.com/wp-content/uploads/2014/09/xynlogo.png")
+    print "Adding " + y_naija_gossip.get_name() + " scraper..."
+    scrapers.append(y_naija_gossip)
+    linda_ikeji = Scraper("Linda Ikeji", api_id="3urqe5fa", category="Gossip", db_collection=collection,
+                          logo="")
+    print "Adding " + linda_ikeji.get_name() + " scraper..."
+    scrapers.append(linda_ikeji)
+    radar_ng = Scraper("Radar NG", api_id="6g898ewm", category="Gossip", db_collection=collection,
+                       logo="http://static.radar.ng/wp-content/uploads/2014/05/logo-small2.png")
+    print "Adding " + radar_ng.get_name() + " scraper..."
+    scrapers.append(radar_ng)
+
+    # Headline sources
+    today_ng_headlines = Scraper("Today NG", api_id="68dq8a2w", category="Headlines", db_collection=collection,
+                                 logo="http://static.today.ng/wp-content/uploads/2015/01/logo.jpg")
+    print "Adding " + today_ng_headlines.get_name() + " scraper..."
+    scrapers.append(today_ng_headlines)
+    vanguard_headlines = Scraper("Vanguard", api_id="abzmopk8", category="Headlines", db_collection=collection,
+        logo="http://cdn1.vanguardngr.com/wp-content/uploads/2013/12/250x55xvanguardlogo.png.pagespeed.ic.WF70w5uJ9P.png")
+    print "Adding " + vanguard_headlines.get_name() + " scraper..."
+    scrapers.append(vanguard_headlines)
+    y_naija_headlines = Scraper("Y Naija", api_id="e6jhynpm", category="Headlines", db_collection=collection,
+                                logo="http://ynaija.com/wp-content/uploads/2014/05/logo.png")
+    print "Adding " + y_naija_headlines.get_name() + " scraper..."
+    scrapers.append(y_naija_headlines)
+    sahara = SaharaScraper("Sahara Reporters", api_id="8wd4g63i", category="Headlines", db_collection=collection,
+                logo="http://saharareporters.com/sites/default/themes/sr_theme/images/layout/header/header-logo.png")
+    print "Adding " + sahara.get_name() + " scraper..."
+    scrapers.append(sahara)
+
+    # Tech
+    today_ng_tech = Scraper("Today NG", api_id="2yio1io4", category="Tech", db_collection=collection,
+                            logo="http://static.today.ng/wp-content/uploads/2014/09/logo.jpg")
+    print "Adding " + today_ng_tech.get_name() + " scraper..."
+    scrapers.append(today_ng_tech)
+    tech_cabal = Scraper("Tech Cabal", api_id="3lfnkuxu", category="Tech", db_collection=collection,
+                         logo="http://www.techcabal.com/wp-content/uploads/2013/09/tclogobig.png")
+    print "Adding " + tech_cabal.get_name() + " scraper..."
     scrapers.append(tech_cabal)
+
+    # Business
+    ventures = Scraper("Ventures Africa", api_id="7l5zbtl6", category="Business", db_collection=collection,
+                       logo="http://www.ventures-africa.com/wp-content/uploads/2013/12/logo_web.png")
+    print "Adding " + ventures.get_name() + " scraper..."
+    scrapers.append(ventures)
+    vanguard_business = Scraper("Vanguard", api_id="cskj1sxo", category="Business", db_collection=collection,
+        logo="http://cdn1.vanguardngr.com/wp-content/uploads/2013/12/250x55xvanguardlogo.png.pagespeed.ic.WF70w5uJ9P.png")
+    print "Adding " + vanguard_business.get_name() + " scraper..."
+    scrapers.append(vanguard_business)
+    punch_biz = Scraper("Punch", api_id="bdid5uka", category="Business", db_collection=collection,
+                        logo="http://punch.cdn.ng/wp-content/themes/punch/images/punch_logo.jpg")
+    print "Adding " + punch_biz.get_name() + " scraper..."
+    scrapers.append(punch_biz)
 
 
 def run_scrapers():
@@ -53,17 +109,7 @@ if __name__ == '__main__':
     scheduler = BlockingScheduler()
     url = os.environ.get('DATABASE_URL')
     jobstores = {'default': MemoryJobStore()}
-    # if url != None:
-    #     jobstores = {
-    #         'default': SQLAlchemyJobStore(url=url)
-    #     }
-    # else:
-    #     jobstores = {
-    #         'default': MemoryJobStore()
-    #     }
-    executors = {
-        'processpool': ProcessPoolExecutor(max_workers=1)
-    }
+    executors = {'processpool': ProcessPoolExecutor(max_workers=1)}
     scheduler.configure(jobstores=jobstores, executors=executors, timezone=timezone('US/Eastern'))
     scheduler.add_listener(my_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
     scheduler.add_job(scrape, trigger='cron', hour='*')
