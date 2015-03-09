71f0595a04e57684da163c5030b2498d097a7304
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-03-08 20:34:08 -0400

    Refactored scrapers
    Changed BaseScraper to Scraper and overhauled its implementation

---
+++ scraper.py
@@ -0,0 +1,46 @@
+import json
+import os
+import urllib
+import datetime
+
+KIMONO_API_KEY = os.environ.get('KIMONO_API_KEY')
+
+
+class Scraper:
+    def __init__(self, name, api_id, category, db_collection, logo=""):
+        self.mix_index = 0
+        self.name = name
+        self.api_id = api_id
+        self.category = category
+        self.logo = logo
+        self.db_collection = db_collection
+
+    def get_name(self):
+        return self.name
+
+    def load_data(self):
+        results = json.load(
+            urllib.urlopen("https://www.kimonolabs.com/api/"+self.api_id+"?apikey="+KIMONO_API_KEY)
+        )
+        return results
+
+    def run(self):
+        results = self.load_data()
+        count = 0
+        while count < results["count"]:
+            piece = results["results"]["collection1"][count]
+            if self.db_collection.find({"source": piece["source"]}).count() != 0:
+                return
+            else:
+                self.db_collection.insert({
+                    "title": piece["title"],
+                    "source": piece["source"],
+                    "coverPic": piece["coverPic"],
+                    "section": self.category,
+                    "logo": self.logo,
+                    "popularity": 0,
+                    "mixIndex": self.mix_index,
+                    "dateAdded": datetime.datetime.now()
+                })
+            self.mix_index = count % results["count"]
+            count += 1
